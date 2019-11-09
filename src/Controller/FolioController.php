194f543f6b19715e703d8410443d198071632948
fabienchapuis <fabien-chapuis@netc.fr>
2019-11-09 16:32:45 +0000

    creation CRUD

--- src/Controller/FolioController.php
+++ src/Controller/FolioController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+
+use App\Repository\PropertyRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FolioController extends AbstractController
 {
+
+
+
 /**
  * @Route("/folio", name="folio")
- * 
+ * @param PropertyRepository $repository
+ * @return Response
  */
-public function index()
+public function index(PropertyRepository $repository): Response
 {
-    return $this->render('folio.html.twig');
+    $properties = $repository->findAll();
+    
+    return $this->render('folio.html.twig',[
+        'properties' => $properties
+    ]);
 }
 
 
