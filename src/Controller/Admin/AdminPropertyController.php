94cdbf1dc1561d0062c210f97ac3e987c7fb5359
fabienchapuis <fabien-chapuis@netc.fr>
2019-11-09 23:51:15 +0000

    fin |->  CRUD

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -8,6 +8,9 @@ use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Flex\Response as SymfonyResponse;
+use \Symfony\Component\HttpFoundation\RedirectResponse;
 
 class AdminPropertyController extends AbstractController{
 
@@ -55,6 +58,7 @@ class AdminPropertyController extends AbstractController{
         if ($form->isSubmitted() && $form->isValid()){
             $this->em->persist($property);
             $this->em->flush();
+            $this->addFlash('success','Projet créer !');
             return $this->redirectToRoute('admin.property.index');
         }
 
@@ -67,7 +71,7 @@ class AdminPropertyController extends AbstractController{
 
 
 /**
- * @Route("/admin/property/{id}" , name="admin.property.edit")
+ * @Route("/admin/property/{id}" , name="admin.property.edit", methods="GET|POST")
  * @param Property $property
  * @param Request $request
  * @return \Symfony\Component\HttpFoundation\Response
@@ -80,6 +84,7 @@ class AdminPropertyController extends AbstractController{
 
         if ($form->isSubmitted() && $form->isValid()){
             $this->em->flush();
+            $this->addFlash('success','Projet modifié !');
             return $this->redirectToRoute('admin.property.index');
         }
 
@@ -89,7 +94,22 @@ class AdminPropertyController extends AbstractController{
             'form' => $form->createView()
         ]);
     }
+/**
+ * 
+ * @Route("/admin/property/{id}" , name="admin.property.delete", methods="DELETE")
+ * @param Property $property
+ * @param Request $request
+ * @return \Symfony\Component\HttpFoundation\Response
+ */
 
 
+    public function delete(Property $property, Request $request){
+        if($this->isCsrfTokenValid('delete' . $property->getId(), $request->get('_token'))){
+        $this->em->remove($property);
+        $this->em->flush();
+        $this->addFlash('success','Projet supprimé !!');
+    }
+        return $this->redirectToRoute('admin.property.index');
+    }
 
 }
