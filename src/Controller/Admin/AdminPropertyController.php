e66fa5ce2d66fbdd2562fa041e7acb2143bbc7f3
fabienchapuis <fabien-chapuis@netc.fr>
2019-11-09 17:50:42 +0000

    creation CRUD->fonction new

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -43,10 +43,33 @@ class AdminPropertyController extends AbstractController{
         return $this->render('admin/property/index.html.twig',compact('properties'));
     }
 
+
 /**
- * @Route("/admin/{id}" , name="admin.property.edit")
+ * @Route("/admin/property/create", name="admin.property.edit )
+ */
+    public function new( Request $request){
+        $property = new Property();
+        $form = $this->createForm(PropertyType::class, $property);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $this->em->persist($property);
+            $this->em->flush();
+            return $this->redirectToRoute('admin.property.index');
+        }
+
+
+        return $this->render('admin/property/new.html.twig',[
+            'property' => $property,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+/**
+ * @Route("/admin/property/{id}" , name="admin.property.edit")
  * @param Property $property
- * @param Resquest $request
+ * @param Request $request
  * @return \Symfony\Component\HttpFoundation\Response
  */
 
