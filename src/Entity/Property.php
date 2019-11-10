1ca470743907c3b5271b36206d2d92d284a8bf3e
fabienchapuis <fabien-chapuis@netc.fr>
2019-11-10 17:48:19 +0000

    configuration image uploader

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -3,11 +3,17 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\File\File;
+
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PropertyRepository")
+ * @Vich\Uploadable
  */
 class Property
 {
@@ -17,6 +23,27 @@ class Property
      * @ORM\Column(type="integer")
      */
     private $id;
+
+/**
+ * @var string|null
+ * @ORM\Column(type="string", length=255)
+ */
+private $filename;
+
+/**
+ * @var File|null
+ * @Vich\UploadableField(mapping="property_image", fileNameProperty="filename")
+ * @Assert\Image(
+     *     minWidth = "500",
+     *     minWidthMessage = "The image width is too small ({{ width }}px). Minimum width expected is {{ min_width }}px",   
+     *     minHeight = "350",
+     *     minHeightMessage = "The image height is too small ({{ height }}px). Minimum height expected is {{ min_height }}px",
+     *     mimeTypes = {"image/jpeg", "image/png","image/jpg"},
+     *     mimeTypesMessage = "Only .jpeg .png .jpg  Extension valide"
+     * )
+ */
+private $imageFile;
+
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -43,6 +70,12 @@ class Property
      * @ORM\Column(type="date", nullable=false)
      */
     private $date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime|null
+     */
+    private $updated_at;
 
     public function getId(): ?int
     {
@@ -90,6 +123,78 @@ class Property
     public function setUrl(string $url): self
     {
         $this->url = $url;
+        return $this;
+    }
+
+
+
+/**
+ * Get the value of filename
+ *
+ * @return  string|null
+ */ 
+public function getFilename(): ?string
+{
+return $this->filename;
+}
+
+/**
+ * Set the value of filename
+ *
+ * @param  string|null  $filename
+ *
+ * @return  Property
+ */ 
+public function setFilename(?string $filename): Property
+{
+$this->filename = $filename;
+
+return $this;
+}
+
+/**
+ * Get the value of imageFile
+ *
+ * @return  File|null
+ */ 
+public function getImageFile(): ?File
+{
+return $this->imageFile;
+}
+
+/**
+ * Set the value of imageFile
+ *
+ * @param  File|null  $imageFile
+ *
+ * @return  Property
+ */ 
+public function setImageFile(?File $imageFile): Property
+{
+$this->imageFile = $imageFile;
+if ($this->imageFile instanceof UploadedFile) {
+    $this->updated_at = new \DateTime('now');
+}
+return $this;
+}
+
+    /**
+     * Get the value of updated_at
+     */ 
+    public function getUpdated_at()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @return  self
+     */ 
+    public function setUpdated_at($updated_at)
+    {
+        $this->updated_at = $updated_at;
+
         return $this;
     }
 }
--- src/Form/PropertyType.php
+++ src/Form/PropertyType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Property;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,6 +17,9 @@ class PropertyType extends AbstractType
             ->add('description')
             ->add('url')
             ->add('date')
+            ->add('imageFile',FileType::class,[
+                'required' => false 
+            ])
         ;
     }
 
