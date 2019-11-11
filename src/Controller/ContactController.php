34c1ce5dece9e4957f65c96a7690ae373f8f63ab
fabienchapuis <fabien-chapuis@netc.fr>
2019-11-11 00:17:02 +0000

    instalation + essai mail

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,23 +2,33 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
+use App\Entity\Contact;
+use App\Notification\ContactNotification;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ContactController extends AbstractController
 {
 /**
  * @Route("/contact", name="contact")
- * 
+ *  @return Response
  */
-public function index()
+public function index(Request $request,ContactNotification $notification): Response
 {
-    return $this->render('contact.html.twig');
-}
+    $contact = new Contact();
+    
+    $form = $this->createForm(ContactType::class, $contact);
+    $form->handleRequest($request);
 
-
-
-
-
-
-}
+    if ($form->isSubmitted() && $form->isValid()){
+        $notification->notify($contact);
+        $this->addFlash('success','Votre email a bien été envoyé');
+        
+    return $this->redirectToRoute('contact.html.twig',[
+        'form' => $form->createView()]);
+    
+        }
+}}
